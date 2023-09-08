f49a697b1183097ff75f0750db3e77d17bae5cc5
AmirBasuti <95620296+AmirBasuti@users.noreply.github.com>
2023-09-08 20:54:27 +0330

    Update keys.py
    "Fix the name for uploading in the server (server doesn't support Persian)."

--- keys.py
+++ keys.py
@@ -9,12 +9,12 @@ for person_name in valid_person.keys() :person_keyboard.insert(person_name)
 
 
 course_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-valid_course = ["احتمال" ,
-                "مباني جبر" ,
-                "مباني آناليز عددي"  ,
-                "مباني نظريه محاسبه" ,
-                "مباني منطق ونظريه مجموعه ها" ]
-for course in valid_course :course_keyboard.insert(course)
+valid_course = {"احتمال":"ehtemal" ,
+                "مباني جبر":"mabani_jabr" ,
+                "مباني آناليز عددي" : "analiz_adadi",
+                "مباني نظريه محاسبه":"nazarie_mohasebe" ,
+                "مباني منطق ونظريه مجموعه ها":"manteq_nazarie_majmoeha" }
+for course in valid_course.keys() :course_keyboard.insert(course)
 
 bye_me_coffee = InlineKeyboardMarkup().add(
     InlineKeyboardButton(text="برای من قهوه بخر",
